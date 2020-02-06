6e5c8431dbad14f59105f43e40bf9fa7299cf46d
vikku088 <viveksharma490@gmail.com>
2020-02-07 02:45:19 +0530

    Link and NavLink use

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import {Link, NavLink} from 'react-router-dom';
 const Nav = () =>{
     return (
         <nav className = "nav-wrapper red darken-3">
             <div className = "conatiner">
                 <a href = "" className = "brand-logo">Web Page</a>
                 <ul className = "right">
-                    <li><a href = "/">Home</a></li>
-                    <li><a href = "/about">About</a></li>
-                    <li><a href = "/contact">Contact</a></li>
+                    <li><NavLink to = "/">Home</NavLink></li>
+                    <li><NavLink to = "/about">About</NavLink></li>
+                    <li><NavLink to = "/contact">Contact</NavLink></li>
                 </ul>
             </div>
 
